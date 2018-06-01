5b33455f96150307a56b36d28c09951a289ec93e
Milan Humagain <humagm@g.toiohomai.ac.nz>
2018-06-01 13:30:00 +1200

    Q3L23,24,stoppage  method will calculate the time taken by each array.

--- question03/Program.cs
+++ question03/Program.cs
@@ -15,9 +15,14 @@ namespace question03
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();// Start Method
             
-            foreach(var fruit in fruitsArray) {
+            foreach(var fruit in fruitsArray) //it is  to calculated total time taken by array.
+            {
                 Console.WriteLine(fruit);
             }
+
+            stopWatch.Stop();// time stopping method
+            System.Console.WriteLine($"Total time it took is  {stopWatch.ElapsedMilliseconds} milsecs");//this method will calculate the time taken by each array.  
+
         }
     }
 }
