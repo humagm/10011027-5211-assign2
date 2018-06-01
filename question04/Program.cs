9847b9a14bac5550bb599a37df3dcfb234ecc7a4
Milan Humagain <humagm@g.toiohomai.ac.nz>
2018-06-01 15:34:23 +1200

    Q4L11, add method Int32.Parse cause it is equivalent to passing value

--- question04/Program.cs
+++ question04/Program.cs
@@ -8,7 +8,7 @@ namespace question04
         {
             Console.WriteLine("Print out the reverse timestables (highest number first) for:");
 
-            var number = Console.ReadLine();
+            var number = Int32.Parse(Console.ReadLine());
 
             for(var i = 0; i < 12; i++) {
                 var a = i + 1;
